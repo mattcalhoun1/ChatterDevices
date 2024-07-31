c6e0e35023fca82a19f6997c3acb40689ed0af8e
Matt <mattcalhoun1@gmail.com>
2024-07-31 07:40:11 -0400

    colors global

---
+++ Communicator/src/globals/Globals_Colors.h
@@ -0,0 +1,50 @@
+#ifndef GLOBALS_COLORS
+#define GLOBALS_COLORS
+
+// Color definitions
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+#define GRAY            0x1111
+
+#define DARKBLUE 0x0B16
+#define LIGHTBLUE 0x451B
+#define BEIGE 0xF779
+
+#define DARKGREEN 0x12AA// 0x43AD//0xA613 // rgb(161, 195, 152)
+#define LIGHTGREEN 0xC758 // rgb(198, 235, 197)
+#define DARKRED 0xA208 // rgb(163, 67, 67)
+
+#define LIGHTGREY 0xC618   ///< 198, 195, 198
+
+#define BRIGHTGREEN 0x43AD // rgb(67, 118, 108)
+#define DARKGRAY 0x7BEF    ///< 123, 125, 123
+#define BRIGHTYELLOW 0xfcc0 // rgb(255, 152, 0)
+#define BROWN 0xD343
+#define ORANGE 0xFA20
+
+// tvt colors
+#define ILI9341_BLACK 0x0000       ///<   0,   0,   0
+#define ILI9341_NAVY 0x000F        ///<   0,   0, 123
+#define ILI9341_DARKGREEN 0x03E0   ///<   0, 125,   0
+#define ILI9341_DARKCYAN 0x03EF    ///<   0, 125, 123
+#define ILI9341_MAROON 0x7800      ///< 123,   0,   0
+#define ILI9341_PURPLE 0x780F      ///< 123,   0, 123
+#define ILI9341_OLIVE 0x7BE0       ///< 123, 125,   0
+#define ILI9341_BLUE 0x001F        ///<   0,   0, 255
+#define ILI9341_GREEN 0x07E0       ///<   0, 255,   0
+#define ILI9341_CYAN 0x07FF        ///<   0, 255, 255
+#define ILI9341_RED 0xF800         ///< 255,   0,   0
+#define ILI9341_MAGENTA 0xF81F     ///< 255,   0, 255
+#define ILI9341_YELLOW 0xFFE0      ///< 255, 255,   0
+#define ILI9341_WHITE 0xFFFF       ///< 255, 255, 255
+#define ILI9341_ORANGE 0xFD20      ///< 255, 165,   0
+#define ILI9341_GREENYELLOW 0xAFE5 ///< 173, 255,  41
+#define ILI9341_PINK 0xFC18        ///< 255, 130, 198
+
+#endif
